680747c5487e0ae29a5273dc01ce63553cef503e
Danick Triantis <danick@triantis.nl>
2026-04-09 00:15:25 +0200

    Implement fixed request rate limiter

--- api/request.go
+++ api/request.go
@@ -1,11 +1,23 @@
 package api
 
 import (
+	"context"
 	"io"
 	"net/http"
+	"time"
+
+	"golang.org/x/time/rate"
 )
 
+// limiter ensures that there are at most 1000 requests per 60 seconds.
+var limiter = rate.NewLimiter(rate.Every(60*time.Second/1000), 1)
+
 func SendRequest(query string) ([]byte, error) {
+	err := limiter.Wait(context.Background())
+	if err != nil {
+		return []byte{}, err
+	}
+
 	req, err := http.NewRequest(http.MethodGet, query, nil)
 	if err != nil {
 		return []byte{}, err
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/CaptainKills/xtream-api
 
 go 1.26.1
+
+require golang.org/x/time v0.15.0
